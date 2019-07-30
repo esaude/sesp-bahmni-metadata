729c79f84e3b96841455889d0978fb26ea235054
rajasi <rajasijaywant3129@gmail.com>
2019-07-30 18:01:45 +0530

    Rajasi | MPOC-421 Translation in code

---
+++ sql/postgresql/Update Test Name.sql
@@ -0,0 +1,70 @@
+UPDATE test SET name = 'LDH-TOTAL(Upto245UI/L)' where id=1441;
+
+UPDATE test SET name = 'TOTALLIP.(4.0-10mmol/L)' where id=1442;
+
+UPDATE test SET name = 'Other:' where id=1443;
+
+UPDATE test SET name = 'POTASSIUM(3.3-5.6mmol/L)' where id=1444;
+
+UPDATE test SET name = 'PROT.TOTAL(60-80g/L)' where id=1445;
+
+UPDATE test SET name = 'BSF(upto6%)' where id=1446;
+
+UPDATE test SET name = 'SODIUM(138-151mol/L)' where id=1447;
+
+UPDATE test SET name = 'TRANS.OXAL(Upto40U)' where id=1448;
+
+UPDATE test SET name = 'TRANS.PIRUV.(UptoU)' where id=1449;
+
+UPDATE test SET name = 'TRIGLICER(0,6-2,23mmol/L)' where id=1450;
+
+UPDATE test SET name = 'UREA(3.3-6.7mmol/L)' where id=1451;
+
+UPDATE test SET name = 'HB' where id=1440;
+
+UPDATE test SET name = 'GLOBULIN(25-35G/L)' where id=1439;
+
+UPDATE test SET name = 'GLYCEMIA(3.05-6.05mmol/L))' where id=1438;
+
+UPDATE test SET name = 'GLIC.EXP.' where id=1437;
+
+UPDATE test SET name = 'PHOSP.INOR.(0-77-1.36mmol/L)' where id=1436;
+
+UPDATE test SET name = 'PHOSP.ALCAL(30-80UI/L)' where id=1435;
+
+UPDATE test SET name = 'PHOSP.ÁCIDA(0-7.1UI-L)' where id=1434;
+
+UPDATE test SET name = 'CREATININE(4.2-132Hmol/L)' where id=1433;
+
+UPDATE test SET name = 'CPK(Upto50UI/L)' where id=1432;
+
+UPDATE test SET name = 'CHOLESTER(3,9-6,7mmol/L)' where id=1431;
+
+UPDATE test SET name = 'CHLORIDE(96-110mmol/L)' where id=1430;
+
+UPDATE test SET name = 'CD 4' where id=1429;
+
+UPDATE test SET name = 'ViralLoad' where id=1428;
+
+UPDATE test SET name = 'CALCIUM(2.122.62mmol/L)' where id=1427;
+
+UPDATE test SET name = 'C.GLIC.SIMP' where id=1426;
+
+UPDATE test SET name = 'T-BIL(3.4-21.0Hmol/L)' where id=1425;
+
+UPDATE test SET name = 'BILCONJUG.(Upto8.2Hmol/L)' where id=1424;
+
+UPDATE test SET name = 'AST' where id=1423;
+
+UPDATE test SET name = 'AMILASE(600-1600/UL)' where id=1422;
+
+UPDATE test SET name = 'ALBUMIN(35-50g/L)' where id=1420;
+
+UPDATE test SET name = 'URICACID(172-400mol/L)' where id=1419;
+
+UPDATE test SET name = 'ALT' where id=1421;
+
+UPDATE test SET description = name;
+
+UPDATE organization SET name='Laboratório Externo' where id=1283;
+
