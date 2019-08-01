5a7fa9feab887cd76e4daaec8a9e572bd2d67cee
rajasi <rajasijaywant3129@gmail.com>
2019-08-01 11:41:29 +0530

    Rajasi | MPOC-421_Updated

--- sql/postgresql/Hide Department.sql
+++ sql/postgresql/Hide Department.sql
@@ -1,3 +1,3 @@
 UPDATE test_section SET is_active= 'N';
 
-UPDATE test_section SET is_active= 'Y',name='Departamento de Hematologia',description='Departamento de Hematologia' where description= 'Heamatology Department';
+UPDATE test_section SET is_active= 'Y',name='Departamento de Hematologia',description='Departamento de Hematologia' where description= 'Haematology Department';
