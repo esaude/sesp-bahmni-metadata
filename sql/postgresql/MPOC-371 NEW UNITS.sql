fc79d614a677348debd9e8a089976d422a2920b0
rajasi <rajasijaywant3129@gmail.com>
2019-08-13 17:39:17 +0530

    Rajasi | MPOC-371 Setup Test

---
+++ sql/postgresql/MPOC-371 NEW UNITS.sql
@@ -0,0 +1,20 @@
+INSERT INTO clinlims.unit_of_measure (id, name, description, lastupdated) VALUES (nextval('unit_of_measure_seq'), 'mol/L', ' ', localtimestamp);
+
+INSERT INTO clinlims.unit_of_measure (id, name, description, lastupdated) VALUES (nextval('unit_of_measure_seq'), 'g/L', ' ', localtimestamp);
+
+INSERT INTO clinlims.unit_of_measure (id, name, description, lastupdated) VALUES (nextval('unit_of_measure_seq'), 'U/l', ' ', localtimestamp);
+
+INSERT INTO clinlims.unit_of_measure (id, name, description, lastupdated) VALUES (nextval('unit_of_measure_seq'), 'UL', ' ', localtimestamp);
+
+INSERT INTO clinlims.unit_of_measure (id, name, description, lastupdated) VALUES (nextval('unit_of_measure_seq'), 'Hmol/L', ' ', localtimestamp);
+
+INSERT INTO clinlims.unit_of_measure (id, name, description, lastupdated) VALUES (nextval('unit_of_measure_seq'), 'mmol/l', ' ', localtimestamp);
+
+INSERT INTO clinlims.unit_of_measure (id, name, description, lastupdated) VALUES (nextval('unit_of_measure_seq'), 'Ul/L', ' ', localtimestamp);
+
+INSERT INTO clinlims.unit_of_measure (id, name, description, lastupdated) VALUES (nextval('unit_of_measure_seq'), 'U', ' ', localtimestamp);
+
+INSERT INTO clinlims.unit_of_measure (id, name, description, lastupdated) VALUES (nextval('unit_of_measure_seq'), 'cópias/ml', ' ', localtimestamp);
+
+INSERT INTO clinlims.unit_of_measure (id, name, description, lastupdated) VALUES (nextval('unit_of_measure_seq'), 'células', ' ', localtimestamp);
+
