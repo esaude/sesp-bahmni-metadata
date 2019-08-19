09633bcfad0951ccc958ee4cb2ce28c9ae19e4db
rajasi <rajasijaywant3129@gmail.com>
2019-08-19 10:18:44 +0530

    Rajasi | MPOC-421 Updated

---
+++ sql/postgresql/sample.sql
@@ -0,0 +1,7 @@
+update sample_source set name='Emergência' where description='Emergency';
+
+update sample_source set name='Subcentro' where description='Sub Center';
+
+update sample_source set name='Programa Comunitário' where description='Community Program';
+
+update sample_source set name='balcão de recepção' where description='Registration Desk';
