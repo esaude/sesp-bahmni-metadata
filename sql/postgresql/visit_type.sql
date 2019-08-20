fabf4d59fe1d6bb0c07764ad2cb2a14a4b6d9e61
rajasi <rajasijaywant3129@gmail.com>
2019-08-20 17:30:21 +0530

    Rajasi | MPOC_421 Removed Visits

--- sql/postgresql/visit_type.sql
+++ sql/postgresql/visit_type.sql
@@ -1,11 +1,11 @@
-delete from sample_source where description='OutPatient Department';
+update sample_source set active='f' where description='InPatient Department';
 
-delete from sample_source where description='InPatient Department';
+update sample_source set active='f' where description='OutPatient Department';
 
-delete from sample_source where description='Emergency';
+update sample_source set active='f' where description='Emergency';
 
-delete from sample_source where description='Sub Center';
+update sample_source set active='f' where description='Sub Center';
 
-delete from sample_source where description ='Community Program';
+update sample_source set active='f' where description='Community Program';
 
-delete from sample_source where description='Registration Desk';
+
