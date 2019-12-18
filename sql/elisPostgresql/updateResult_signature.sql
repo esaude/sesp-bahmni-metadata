ae40016cee517bf372359a1eab24f1b3838a25c3
Joao machiana <joaomachiana@gmail.com>
2019-12-18 22:24:18 +0200

    Revert "Merge pull request #240 from esaude/develop"
    This reverts commit dd15220976112a6d80eea4b16b08d7c866b48440, reversing
    changes made to f5df05b33ffd7485264ca48ad5d03ca3cdd31a4e.

--- sql/elisPostgresql/updateResult_signature.sql
+++
@@ -1,5 +0,0 @@
-
-ALTER TABLE system_user ALTER COLUMN last_name TYPE character VARYING(50);
-ALTER TABLE system_user ALTER COLUMN first_name TYPE character VARYING(50);
-
-ALTER TABLE result_signature ALTER COLUMN non_user_name TYPE character VARYING(101);
